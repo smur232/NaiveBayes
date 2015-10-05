f522f717a16a0be01551d83c903e2ece4f0e73f1
smur232 <sarah.d.murata@gmail.com>
2015-10-06 10:45:26 +1300

    creates pickle files to store the word counts each time we read a new article

--- web_crawler.py
+++ web_crawler.py
@@ -2,11 +2,12 @@ import requests
 import re
 import time
 import pickle
+import string
+import traceback
 
 from collections import deque
 from collections import Counter
 from bs4 import BeautifulSoup
-from trying_pickle import whats_in_my_pickle
 
 
 def begin_crawling_from_this_page(url, max_num_of_articles=20):
@@ -16,8 +17,8 @@ def begin_crawling_from_this_page(url, max_num_of_articles=20):
     links = deque()
     links.append(url)
     count = 0
-    pages_visited = get_pages_visited()
-    articles_read_for_category = get_article_counts()
+    pages_visited = read_object_from('visited_pages_set.p', set)
+    articles_read_for_category = read_object_from('articles_read_counter.p', Counter)
     print('pages visited before: ', pages_visited)
     print('articles read before: ', articles_read_for_category)
 
@@ -37,61 +38,51 @@ def begin_crawling_from_this_page(url, max_num_of_articles=20):
             count += 1
 
     pickle.dump(articles_read_for_category, open('articles_read_counter.p', 'wb'))
-    pickle.dump(pages_visited, open('url_visited_sites_set.p', 'wb'))
+    pickle.dump(pages_visited, open('visited_pages_set.p', 'wb'))
     return
-
-def get_pages_visited():
-
-    try:
-        return pickle.load(open('url_visited_sites_set.p', 'rb')) # just in case the input was not a set
-
-    except FileNotFoundError:
-        return set()
-
-
-def get_article_counts():
-    try:
-        return pickle.load(open('articles_read_counter.p', 'rb'))
-    except FileNotFoundError:
-        return Counter()
 
 
 def write_body_to_file(url,links):
-    file_to_write_to = determine_category_file(url)
+    # トレイニングデータを作るために使います
+    article_category = determine_category_file(url)
 
-    if file_to_write_to == 'ignore':
+    if article_category == 'ignore':
         print('This url was ignored:', url)
         return
 
     print('Currently going through ', url, ':')
-    f = open(file_to_write_to, 'a')
+    f = open(article_category + '.csv', 'a')
 
     try:
         r = requests.get(url)
         r.raise_for_status()
         soup = BeautifulSoup(r.text, "html.parser")
-        # it is getting a div called component__tweet!!
         links.extend(collect_links(soup))
 
-        pTags = get_all_body_p_tags(soup)
-
-        for pTag in pTags:
+        p_tags = get_all_body_p_tags(soup)
+        a = read_object_from(article_category + '.p', Counter)
+        for pTag in p_tags:
             contents = str(pTag.contents[0])
+
+            # 後で見れるように、CSV ファイルにも書いて、pickle にも文字のカウンターをアップデートします
             if 'href' not in contents and 'span' not in contents:
                 f.write(contents + '\n')
+                a.update(word.strip(string.punctuation).lower() for word in contents.split())
+        pickle.dump(a, open(article_category + '.p', 'wb'))
 
     except AttributeError:
         print('     This page does not have a body article: ', url)
 
     except Exception as e:
-        print('Had some problem parsing through this page: ', url, e)
+        ('Had some problem parsing through this page: ', url, e)
+        traceback.print_exc()
 
     else:
-        print('     successfully written to file', file_to_write_to)
+        print('     successfully written to file', article_category)
 
     finally:
         f.close()
-        return file_to_write_to
+        return article_category
 
 
 def get_all_body_p_tags(soup):
@@ -112,45 +103,68 @@ def path_to_articles(href):
     return href and re.compile("(?<![a-z])/news/[\w-]+-[\d]+").search(href)
 
 
+def read_object_from(filename, object_type):
+    # pickle からオブジェクとを読み取ります、欲しい object_type とマッチしているかをチェックします
+    # object_type　がマッチしなければ、空の object_type を return します
+
+    try:
+        read_object = pickle.load(open(filename, 'rb'))
+        assert type(read_object) is object_type
+        return read_object
+
+    except FileNotFoundError:
+        return object_type()
+
+    except AssertionError:
+        print('         The object read from', filename, 'is not a', object_type, '!')
+        return object_type()
+
+    except Exception:
+        return object_type()
+
+
 def determine_category_file(url):
     if 'entertainment' in url:
-        return 'entertainment.csv'
+        return 'entertainment'
     elif 'business' in url:
-        return 'business.csv'
+        return 'business'
     elif 'australia' in url:
-        return 'australia.csv'
+        return 'australia'
     elif 'asia' in url:
-        return 'asia.csv'
+        return 'asia'
     elif 'technology' in url:
-        return 'technology.csv'
+        return 'technology'
     elif 'europe' in url:
-        return 'europe.csv'
+        return 'europe'
     elif 'middle-east' in url:
-        return 'middle_east.csv'
+        return 'middle_east'
     elif 'latin-america' in url:
-        return 'latin_america.csv'
+        return 'latin_america'
     elif 'africa' in url:
-        return 'africa.csv'
+        return 'africa'
     elif 'canada' in url:
-        return 'us_canada.csv'
+        return 'us_canada'
     elif 'science' in url:
-        return 'science.csv'
+        return 'science'
     elif 'uk' in url:
-        return 'uk.csv'
+        return 'uk'
     elif 'education' in url:
-        return 'education.csv'
+        return 'education'
     else:
         return 'ignore'
 
 
 def whats_in_my_pickle():
-    a = get_article_counts()
-    b = get_pages_visited()
-    print('Total number of articles: ', sum(a.values()))
-    print('Total number of links: ', len(b))
+    print("\nWhat's in my pickle objects? ")
+    a = read_object_from('articles_read_counter.p', Counter)
+    b = read_object_from('visited_pages_set.p', set)
+    c = read_object_from('europe.p', Counter)
+    print(' Total number of articles:', sum(a.values()))
+    print(' Total number of links:', len(b))
     print(a)
     print(b)
+    print(c)
 
 
-begin_crawling_from_this_page('http://www.bbc.com/news/world-asia-34398371', 10)
+#begin_crawling_from_this_page('http://www.bbc.com/news/world-europe-34442121', 2)
 whats_in_my_pickle()
